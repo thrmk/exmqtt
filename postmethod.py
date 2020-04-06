2e5f4ce5e01453885d321c7991c692cb43c16e74
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-04-06 11:11:39 +0530

    Update postmethod.py

--- postmethod.py
+++ postmethod.py
@@ -245,7 +245,7 @@ time.sleep(1)
 
 subtop="tracker/device/sub"
 pubtop="tracker/device/pub"
-client.connect("test.mosquitto.org",1883)
+client.connect("test.mosquitto.org",8081)
 #client.connect("157.48.149.158",1883)
 #client.connect("exmqtt.herokuapp.com",1883)
 #client.connect("ec2-35-162-194-10.us-west-2.compute.amazonaws.com",1883)
