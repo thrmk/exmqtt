e1d4db292f71579ea8a383706c6d3e140e87e28a
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-04-17 15:16:33 +0530

    Update channel.py

--- channel.py
+++ channel.py
@@ -78,7 +78,8 @@ def on_disconnect(client, userdata, rc):
 #port =443 
 connection = sqlite3.connect('data.db',check_same_thread=False)
 cursor = connection.cursor()
-if cursor.fetchall() is None:
+#if cursor.fetchall() is None:
+if cursor.fetchone()[0]!=1 :
     cursor.execute("CREATE TABLE data (id INTEGER PRIMARY KEY AUTOINCREMENT,stamp VARCHAR(15), devId VARCHAR(15), SPA VARCHAR(15),TA VARCHAR(15) )")
 
 #cursor.execute("CREATE TABLE data (id INTEGER PRIMARY KEY AUTOINCREMENT,stamp VARCHAR(15), devId VARCHAR(15), SPA VARCHAR(15),TA VARCHAR(15) )")
