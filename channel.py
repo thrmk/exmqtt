12ed2b37625a4cb6b8fa870021995fb040851b44
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-04-20 15:59:33 +0530

    Update channel.py

--- channel.py
+++ channel.py
@@ -63,7 +63,7 @@ server = flask.Flask(__name__)
 server.config['DEBUG'] = True
 
 
-server.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+server.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite3:///data.db')
 server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #basedir = os.path.abspath(os.path.dirname(__file__))
 server.secret_key = 'smarttrak'
