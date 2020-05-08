6ee5e06588d2e6f6a47ac8b449d728f1f3afd5a0
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-05-08 13:01:09 +0530

    Update bag.py

--- bag.py
+++ bag.py
@@ -897,4 +897,4 @@ def display_page(pathname):
         
 
 if __name__ == '__main__':
-    app.run_server(debug=True,threaded=True, use_reloader=True)
+    app.run_server(debug=True,threaded=True, use_reloader=False)
